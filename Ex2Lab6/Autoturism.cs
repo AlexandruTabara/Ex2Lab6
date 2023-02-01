519f1058e1cc83a2d235d95ba52f7539335eab6e
Alexandru Tabara <alexandru.tab1@gmail.com>
2023-02-01 12:59:57 +0200

    Add project files.

---
+++ Ex2Lab6/Autoturism.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex2Lab6
+{
+   public class Autoturism
+    {
+        //Campuri care reprezinta caracteristicile unui autoturism
+        private string marca;
+        private string numarDeInmatriculare;
+        private int capacitateCilindrica;
+
+        //Constructorul
+        public Autoturism (string marca, string numarDeInmatriculare, int capacitateCilindrica)
+        {
+            this.marca = marca;
+            this.numarDeInmatriculare = numarDeInmatriculare;
+            this.capacitateCilindrica = capacitateCilindrica;
+        }
+
+        //Metoda care arata printeaza caractersticile autoturismului
+        public void Tiparire()
+        {
+            Console.WriteLine($"{marca}  {numarDeInmatriculare}  {capacitateCilindrica}");
+        }
+    }
+}
---
+++ Ex2Lab6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex2Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creare a 2 obiecte de tip autoturism
+            Autoturism auto1 = new Autoturism("VW", "AB 33 ABC", 1400);
+            Autoturism auto2 = new Autoturism("BMW", "CJ 12 XYZ", 1900);
+
+            //Apelarea metodei de tiparire
+            auto1.Tiparire();
+            auto2.Tiparire();
+        }
+    }
+}
